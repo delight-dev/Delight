b9a63e92b5904c96c1cda3600f9e6853a4faff08
Patrik Opacic <opacic@gmail.com>
2020-08-12 08:04:40 +0200

    Update DelightControlPanel.cs

--- UnityProject/Delight/Assets/Delight/Source/Editor/DelightControlPanel.cs
+++ UnityProject/Delight/Assets/Delight/Source/Editor/DelightControlPanel.cs
@@ -54,11 +54,11 @@ namespace Delight.Editor
                 EditorPrefs.SetBool("Delight_BuildAssetBundles", newBuildAssetBundles);
             }
 
-            var newDeployBuild = EditorGUILayout.Toggle("Rivality Deploy Build", EditorPrefs.GetBool("Delight_DeployBuild"));
-            if (newDeployBuild != deployBuild)
-            {
-                EditorPrefs.SetBool("Delight_DeployBuild", newDeployBuild);
-            }
+            //var newDeployBuild = EditorGUILayout.Toggle("Rivality Deploy Build", EditorPrefs.GetBool("Delight_DeployBuild"));
+            //if (newDeployBuild != deployBuild)
+            //{
+            //    EditorPrefs.SetBool("Delight_DeployBuild", newDeployBuild);
+            //}
 
             bool disableAutoGenerateViews = EditorPrefs.GetBool("Delight_DisableAutoGenerateViews");
             var newDisableAutoGenerateViews = EditorGUILayout.Toggle("Disable view autogen", disableAutoGenerateViews);
