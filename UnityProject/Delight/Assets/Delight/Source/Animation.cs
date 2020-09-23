f0e66aaaa7bd6fe2bacf1b12234adecce5ac1488
Patrik Opacic <opacic@gmail.com>
2020-09-23 18:48:18 +0200

    Bugfix using statement

--- UnityProject/Delight/Assets/Delight/Source/Animation.cs
+++ UnityProject/Delight/Assets/Delight/Source/Animation.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using UnityEngine;
 #endregion
 
